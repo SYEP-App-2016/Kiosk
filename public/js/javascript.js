5e9a8468b36747593b36b4c7cbbff775f5f42066
shirokane <jcanete@aoiths.org>
2016-08-03 14:20:40 -0400

    aaaaaaaaaaaaaaaaaaaaaaaa

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -18,76 +18,85 @@ var a = '<select class="form-control optlist"><% for(var i = 0; i < genre.length
   });
 
 
-  var genre = [
-    "Science Fiction",
-    "Satire",
-    "Drama",
-    "Action and Adventure",
-    "Romance",
-    "Mystery",
-    "Horror",
-    "Self Help",
-    "Health",
-    "Guide",
-    "Travel",
-    "Children's",
-    "Religion, Spirituality & New Age",
-    "Science",
-    "History",
-    "Math",
-    "Anthology",
-    "Poetry",
-    "Encyclopedias",
-    "Dictionary",
-    "Comics / Manga",
-    "Art",
-    "Cookbooks",
-    "Diary",
-    "Journals",
-    "Series",
-    "Trilogy",
-    "Biographies",
-    "Autobiographies",
-    "Fantasy"
-  ];
-
-  console.log(genre.length);
-  console.log( genre.sort() );
-
-  // CAN BE CONVERTED TO D-R-Y
-
-
-  function createGenreOptions(){
-    var l = genre.length,
-        str = "<option>- Select Genre -</option>";
-
-    for(var i = 0; i < l; i++) {
-      str += "<option value='" + i + "'>" + genre[i]+ "</option>";
-    }
-
-    return str;
-  }
-
-
-  // D-R-Y
   function updateHTML(el, content){
-    $(el).append(content);
+  $(el).append(content);
+}
+
+function clickResults(el, callback){
+  $(el).change(callback);
+}
+
+
+
+//
+// KIOSK CODE
+var genre = [
+  "Science Fiction",
+  "Satire",
+  "Drama",
+  "Action and Adventure",
+  "Romance",
+  "Mystery",
+  "Horror",
+  "Self Help",
+  "Health",
+  "Guide",
+  "Travel",
+  "Children's",
+  "Religion, Spirituality & New Age",
+  "Science",
+  "History",
+  "Math",
+  "Anthology",
+  "Poetry",
+  "Encyclopedias",
+  "Dictionary",
+  "Comics / Manga",
+  "Art",
+  "Cookbooks",
+  "Diary",
+  "Journals",
+  "Series",
+  "Trilogy",
+  "Biographies",
+  "Autobiographies",
+  "Fantasy"
+];
+
+// USUAL SETUP
+// GET HOW MANY ELEMENTS
+console.log(genre.length);
+
+
+// SORT ALPHABETICALLY
+console.log( genre.sort() );
+
+
+// STRING BUILDER THAT RETURNS STRING
+function createGenreOptions(){
+  var l = genre.length,
+      str = "<option>- Select Genre -</option>";
+
+  for(var i = 0; i < l; i++) {
+    str += "<option value='" + i + "'>" + genre[i]+ "</option>";
   }
+  return str;
+}
 
 
-  updateHTML("#ddlGenres", createGenreOptions());
+// EVENT LISTENER
+clickResults("#ddlGenres", function(){
+  // PRINT GENRES IN DIV
+  $(".dvSkills").append("<input name='genre' value='"+$("#ddlGenres option:selected").text() + "' style='display: none;'/>");
+
+  // GET NUMBER VALUE / POSITION IN ARRAY
+  $("#hnSkills")[0].value += this.value + ",";
+
+});
 
 
-  // D-R-Y DESIGN PATTERN
-  function clickResults(el, callback){
-    $(el).change(callback);
-  }
+// FIND BY CLASS / ID AND SET CONTENT
+updateHTML("#ddlGenres", createGenreOptions());
 
-  clickResults("#ddlGenres", function(){
-    // $(".dvSkills").append("<span>" + $("#ddlGenres option:selected").text() + ", </span>");
-    $(".dvSkills").append("<input name = 'genre[] 'value='" + $("#ddlGenres option:selected").text() +"'/>");
 
-    $("#hnSkills")[0].value += this.value + ",";
-
-  });
 });
