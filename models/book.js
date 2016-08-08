44bbded49809322c8fcd0e91338bdbd822032fc8
Aaron <Aaron>
2016-08-07 20:21:31 -0400

    added Schema.Types.ObjectId for Id

--- models/book.js
+++ models/book.js
@@ -1,14 +1,16 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-// ADD IMAGE URL
+
 var bookSchema = mongoose.Schema({
-  title: {required : 'true', type : String},
-  author: {required : 'true', type : String},
+  acc_id: mongoose.Schema.Types.ObjectId,
+  title: { required : 'true', type : String},
+  img: String,
+  author: { required : 'true', type : String},
   summary: String,
   pageCount: Number,
   copies: Number,
-  copiesAvailable: Number,
+  copiesAvailable: { required: true, type: Number },
   ratings: Number,
   publisher: String,
   coverType: String,
@@ -16,8 +18,5 @@ var bookSchema = mongoose.Schema({
 });
 
 var Book = mongoose.model('Book', bookSchema);
-
-// var hunger = new Book({title:'The Hunger Games', author: 'lady'});
-
 
 module.exports = Book;
