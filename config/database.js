c64489c8ba01a0571f06b1141ef2b789ff9b10c1
shirokane <jcanete@aoiths.org>
2016-08-16 14:19:38 -0400

    aa

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  //  url: 'mongodb://dba:summeryouth2016@ds027145.mlab.com:27145/cypresshills'
-  url: 'mongodb://localhost:ehosat2U'
+   url: 'mongodb://dba:summeryouth2016@ds027145.mlab.com:27145/cypresshills'
+  // url: 'mongodb://localhost:ehosat2U'
 }
